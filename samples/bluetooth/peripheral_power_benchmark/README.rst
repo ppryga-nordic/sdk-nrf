3c44d810125efe23ec7b3572419ec4b6755cdeed
Piotr Pryga <piotr.pryga@nordicsemi.no>
2025-02-28 15:49:20 +0100

    samples: bluetooth: power benchmark samples
    Adds a test sample applications to collect measurements for
    various adveritsing and connection scenarios on nRF SoCs.
    
    The central is a tester application, that should be used to
    connect to a DK that measurements are collected from.
    
    The peripheral is a DUT application, that should be run on
    a DK to take emasurements.
    
    Signed-off-by: Piotr Pryga <piotr.pryga@nordicsemi.no>

---
+++ samples/bluetooth/peripheral_power_benchmark/README.rst
@@ -0,0 +1,10 @@
+.. _peripheral_power_benchmark:
+
+Bluetooth: Peripheral Power Benchmark
+#####################################
+
+.. contents::
+   :local:
+   :depth: 2
+
+The Peripheral Power Benchmark is a simple application created to make easier measurements of power usage.
---
+++ samples/bluetooth/peripheral_power_benchmark/src/main.c
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+/** @file
+ *  @brief Peripheral Power Benchmark
+ */
+#include <stddef.h>
+#include <string.h>
+#include <errno.h>
+#include <zephyr/kernel.h>
+#include <zephyr/types.h>
+#include <zephyr/sys/printk.h>
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/hci.h>
+
+// Short name to be used for tests of a connection
+#define DEVICE_NAME             "nRF-PM"
+#define DEVICE_NAME_LEN         (sizeof(DEVICE_NAME) - 1)
+
+static const struct bt_data ad[] = {
+#if defined(CONFIG_TEST_PERIPHERAL_CONNECTION)
+	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN)
+#endif /* CONFIG_TEST_PERIPHERAL_CONNECTION */
+};
+
+static struct bt_le_adv_param m_adv_param =	BT_LE_ADV_PARAM_INIT(
+#if defined(CONFIG_TEST_PERIPHERAL_CONNECTION)
+		BT_LE_ADV_OPT_CONN,
+#else
+		BT_LE_ADV_OPT_NONE,
+#endif /* CONFIG_TEST_PERIPHERAL_CONNECTION */
+		CONFIG_ADVERTISING_INTERVAL,
+		CONFIG_ADVERTISING_INTERVAL,
+		NULL);
+
+static void connected(struct bt_conn *conn, uint8_t conn_err)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	if (conn_err) {
+		printk("Connection failed, err 0x%02x %s\n", conn_err, bt_hci_err_to_str(conn_err));
+		return;
+	}
+
+	printk("Connected to: %s\n", addr);
+}
+
+static void disconnected(struct bt_conn *conn, uint8_t reason)
+{
+	int err;
+
+	printk("Disconnected, reason 0x%02x %s\n", reason, bt_hci_err_to_str(reason));
+
+	err = bt_le_adv_start(&m_adv_param, ad, ARRAY_SIZE(ad), NULL, 0);
+	if (err) {
+		printk("Advertising restart failed to start (err %d)\n", err);
+		return;
+	}
+}
+
+BT_CONN_CB_DEFINE(conn_callbacks) = {
+	.connected = connected,
+	.disconnected = disconnected,
+};
+
+int main(void)
+{
+	int err;
+
+	printk("Starting Bluetooth Peripheral Power Benchmark\n");
+
+	err = bt_enable(NULL);
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return 0;
+	}
+
+	printk("Bluetooth initialized\n");
+
+	err = bt_le_adv_start(&m_adv_param, ad, ARRAY_SIZE(ad), NULL, 0);
+	if (err) {
+		printk("Advertising failed to start (err %d)\n", err);
+		return 0;
+	}
+
+	for (;;) {
+		k_sleep(K_FOREVER);
+	}
+}
