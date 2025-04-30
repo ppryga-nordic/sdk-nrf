45b0ffadf7d10d0b1d5f8342a6b0db1d68d5dea2
Piotr Pryga <piotr.pryga@nordicsemi.no>
2025-04-30 07:12:12 +0200

    peripeheral_hr_coded: modify sample to use lib softdevice peripheral
    Modify the sample and its configuration to use features supported
    by lib_softdevice_controller_peripheral. That should limit use
    of FLASH and RAM memory to fit this simple sample into nRF54h20
    radio core local ram.
    
    Signed-off-by: Piotr Pryga <piotr.pryga@nordicsemi.no>

--- samples/bluetooth/peripheral_hr_coded/src/main.c
+++ samples/bluetooth/peripheral_hr_coded/src/main.c
@@ -32,13 +32,11 @@
 #define RUN_LED_BLINK_INTERVAL  1000
 #define NOTIFY_INTERVAL         1000
 
-static void start_advertising_coded(struct k_work *work);
+static void start_advertising(struct k_work *work);
 static void notify_work_handler(struct k_work *work);
 
-static K_WORK_DEFINE(start_advertising_worker, start_advertising_coded);
+static K_WORK_DEFINE(start_advertising_worker, start_advertising);
 static K_WORK_DELAYABLE_DEFINE(notify_work, notify_work_handler);
-
-static struct bt_le_ext_adv *adv;
 
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
@@ -47,26 +45,6 @@ static const struct bt_data ad[] = {
 					  BT_UUID_16_ENCODE(BT_UUID_DIS_VAL)),
 	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN)
 };
-
-
-static const char *phy_to_str(uint8_t phy)
-{
-	switch (phy) {
-	case BT_GAP_LE_PHY_NONE:
-		return "No packets";
-	case BT_GAP_LE_PHY_1M:
-		return "LE 1M";
-	case BT_GAP_LE_PHY_2M:
-		return "LE 2M";
-	case BT_GAP_LE_PHY_CODED:
-		return "LE Coded";
-	case BT_GAP_LE_PHY_CODED_S8:
-		return "S=8 Coded";
-	case BT_GAP_LE_PHY_CODED_S2:
-		return "S=2 Coded";
-	default: return "Unknown";
-	}
-}
 
 static void connected(struct bt_conn *conn, uint8_t conn_err)
 {
@@ -85,11 +63,7 @@ static void connected(struct bt_conn *conn, uint8_t conn_err)
 	if (err) {
 		printk("Failed to get connection info (err %d)\n", err);
 	} else {
-		const struct bt_conn_le_phy_info *phy_info;
-		phy_info = info.le.phy;
-
-		printk("Connected: %s, tx_phy %s, rx_phy %s\n",
-		       addr, phy_to_str(phy_info->tx_phy), phy_to_str(phy_info->rx_phy));
+		printk("Connected: %s\n", addr);
 	}
 
 	dk_set_led_on(CON_STATUS_LED);
@@ -109,46 +83,16 @@ BT_CONN_CB_DEFINE(conn_callbacks) = {
 	.disconnected = disconnected,
 };
 
-static int create_advertising_coded(void)
+static void start_advertising(struct k_work *work)
 {
-	int err;
-	struct bt_le_adv_param param =
-		BT_LE_ADV_PARAM_INIT(BT_LE_ADV_OPT_CONN |
-				     BT_LE_ADV_OPT_EXT_ADV |
-				     BT_LE_ADV_OPT_CODED |
-				     BT_LE_ADV_OPT_REQUIRE_S8_CODING,
-				     BT_GAP_ADV_FAST_INT_MIN_2,
-				     BT_GAP_ADV_FAST_INT_MAX_2,
-				     NULL);
+	int err = bt_le_adv_start(BT_LE_ADV_CONN_FAST_2, ad, ARRAY_SIZE(ad), NULL, 0);
 
-	err = bt_le_ext_adv_create(&param, NULL, &adv);
 	if (err) {
-		printk("Failed to create advertiser set (err %d)\n", err);
-		return err;
-	}
-
-	printk("Created adv: %p\n", adv);
-
-	err = bt_le_ext_adv_set_data(adv, ad, ARRAY_SIZE(ad), NULL, 0);
-	if (err) {
-		printk("Failed to set advertising data (err %d)\n", err);
-		return err;
-	}
-
-	return 0;
-}
-
-static void start_advertising_coded(struct k_work *work)
-{
-	int err;
-
-	err = bt_le_ext_adv_start(adv, BT_LE_EXT_ADV_START_DEFAULT);
-	if (err) {
-		printk("Failed to start advertising set (err %d)\n", err);
+		printk("Advertising failed to start (err %d)\n", err);
 		return;
 	}
 
-	printk("Advertiser %p set started\n", adv);
+	printk("Advertising successfully started\n");
 }
 
 static void bas_notify(void)
@@ -207,12 +151,6 @@ int main(void)
 	}
 
 	printk("Bluetooth initialized\n");
-
-	err = create_advertising_coded();
-	if (err) {
-		printk("Advertising failed to create (err %d)\n", err);
-		return 0;
-	}
 
 	k_work_submit(&start_advertising_worker);
 	k_work_schedule(&notify_work, K_NO_WAIT);
