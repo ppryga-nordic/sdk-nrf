f0930b8a963304adba974ab547dda94fec3751c4
Piotr Pryga <piotr.pryga@nordicsemi.no>
2025-05-02 22:46:50 +0200

    peripheral_hr_coded: Add low power configuration
    Configure to enter low power state (system-all-on-idle).
    
    NOTE: when run with this configs there is still something
    blocking enter to S2IDLE but that seems to be missing
    communication with sysctrl.
    
    Chagnge sample to use it for measurements when a connection
    is established.
    
    Signed-off-by: Piotr Pryga <piotr.pryga@nordicsemi.no>

--- samples/bluetooth/peripheral_hr_coded/src/main.c
+++ samples/bluetooth/peripheral_hr_coded/src/main.c
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2020 Nordic Semiconductor ASA
+ * Copyright (c) 2025 Nordic Semiconductor ASA
  *
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
 /** @file
- *  @brief Peripheral Heart Rate over LE Coded PHY sample
+ *  @brief Peripheral Power Benchmark
  */
 #include <stddef.h>
 #include <string.h>
@@ -16,40 +16,31 @@
 
 #include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/bluetooth/conn.h>
-#include <zephyr/bluetooth/gatt.h>
 #include <zephyr/bluetooth/hci.h>
-#include <zephyr/bluetooth/uuid.h>
-#include <zephyr/bluetooth/services/bas.h>
-#include <zephyr/bluetooth/services/hrs.h>
 
-#include <dk_buttons_and_leds.h>
-
-#define DEVICE_NAME             CONFIG_BT_DEVICE_NAME
+// Short name to be used for tests of a connection
+#define DEVICE_NAME             "nRF-PM"
 #define DEVICE_NAME_LEN         (sizeof(DEVICE_NAME) - 1)
 
-#define RUN_STATUS_LED          DK_LED1
-#define CON_STATUS_LED          DK_LED2
-#define RUN_LED_BLINK_INTERVAL  1000
-#define NOTIFY_INTERVAL         1000
-
-static void start_advertising(struct k_work *work);
-static void notify_work_handler(struct k_work *work);
-
-static K_WORK_DEFINE(start_advertising_worker, start_advertising);
-static K_WORK_DELAYABLE_DEFINE(notify_work, notify_work_handler);
-
 static const struct bt_data ad[] = {
+#if defined(CONFIG_TEST_PERIPHERAL_CONNECTION)
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
-	BT_DATA_BYTES(BT_DATA_UUID16_ALL, BT_UUID_16_ENCODE(BT_UUID_HRS_VAL),
-					  BT_UUID_16_ENCODE(BT_UUID_BAS_VAL),
-					  BT_UUID_16_ENCODE(BT_UUID_DIS_VAL)),
 	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN)
+#endif /* CONFIG_TEST_PERIPHERAL_CONNECTION */
 };
+
+static struct bt_le_adv_param m_adv_param =	BT_LE_ADV_PARAM_INIT(
+#if defined(CONFIG_TEST_PERIPHERAL_CONNECTION)
+		BT_LE_ADV_OPT_CONN,
+#else
+		BT_LE_ADV_OPT_NONE,
+#endif /* CONFIG_TEST_PERIPHERAL_CONNECTION */
+		CONFIG_ADVERTISING_INTERVAL,
+		CONFIG_ADVERTISING_INTERVAL,
+		NULL);
 
 static void connected(struct bt_conn *conn, uint8_t conn_err)
 {
-	int err;
-	struct bt_conn_info info;
 	char addr[BT_ADDR_LE_STR_LEN];
 
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
@@ -59,23 +50,20 @@ static void connected(struct bt_conn *conn, uint8_t conn_err)
 		return;
 	}
 
-	err = bt_conn_get_info(conn, &info);
-	if (err) {
-		printk("Failed to get connection info (err %d)\n", err);
-	} else {
-		printk("Connected: %s\n", addr);
-	}
-
-	dk_set_led_on(CON_STATUS_LED);
+	printk("Connected to: %s\n", addr);
 }
 
 static void disconnected(struct bt_conn *conn, uint8_t reason)
 {
+	int err;
+
 	printk("Disconnected, reason 0x%02x %s\n", reason, bt_hci_err_to_str(reason));
 
-	k_work_submit(&start_advertising_worker);
-
-	dk_set_led_off(CON_STATUS_LED);
+	err = bt_le_adv_start(&m_adv_param, ad, ARRAY_SIZE(ad), NULL, 0);
+	if (err) {
+		printk("Advertising restart failed to start (err %d)\n", err);
+		return;
+	}
 }
 
 BT_CONN_CB_DEFINE(conn_callbacks) = {
@@ -83,66 +71,11 @@ BT_CONN_CB_DEFINE(conn_callbacks) = {
 	.disconnected = disconnected,
 };
 
-static void start_advertising(struct k_work *work)
-{
-	int err = bt_le_adv_start(BT_LE_ADV_CONN_FAST_2, ad, ARRAY_SIZE(ad), NULL, 0);
-
-	if (err) {
-		printk("Advertising failed to start (err %d)\n", err);
-		return;
-	}
-
-	printk("Advertising successfully started\n");
-}
-
-static void bas_notify(void)
-{
-	uint8_t battery_level = bt_bas_get_battery_level();
-
-	__ASSERT_NO_MSG(battery_level > 0);
-
-	battery_level--;
-
-	if (!battery_level) {
-		battery_level = 100;
-	}
-
-	bt_bas_set_battery_level(battery_level);
-}
-
-static void hrs_notify(void)
-{
-	static uint8_t heartrate = 100;
-
-	heartrate++;
-	if (heartrate == 160) {
-		heartrate = 100;
-	}
-
-	bt_hrs_notify(heartrate);
-}
-
-static void notify_work_handler(struct k_work *work)
-{
-	/* Services data simulation. */
-	hrs_notify();
-	bas_notify();
-
-	k_work_reschedule(k_work_delayable_from_work(work), K_MSEC(NOTIFY_INTERVAL));
-}
-
 int main(void)
 {
-	uint32_t led_status = 0;
 	int err;
 
-	printk("Starting Bluetooth Peripheral HR coded sample\n");
-
-	err = dk_leds_init();
-	if (err) {
-		printk("LEDs init failed (err %d)\n", err);
-		return 0;
-	}
+	printk("Starting Bluetooth Peripheral Power Benchmark\n");
 
 	err = bt_enable(NULL);
 	if (err) {
@@ -152,11 +85,13 @@ int main(void)
 
 	printk("Bluetooth initialized\n");
 
-	k_work_submit(&start_advertising_worker);
-	k_work_schedule(&notify_work, K_NO_WAIT);
+	err = bt_le_adv_start(&m_adv_param, ad, ARRAY_SIZE(ad), NULL, 0);
+	if (err) {
+		printk("Advertising failed to start (err %d)\n", err);
+		return 0;
+	}
 
 	for (;;) {
-		dk_set_led(RUN_STATUS_LED, (++led_status) % 2);
-		k_sleep(K_MSEC(RUN_LED_BLINK_INTERVAL));
+		k_sleep(K_FOREVER);
 	}
 }
